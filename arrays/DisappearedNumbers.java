ca0da7c501df9d933c105cabe196bc2d89bdbf6b
Aayush <aayushjha2431@gmail.com>
2025-07-16 11:47:50 +0530

    Starting patterns pdf

---
+++ arrays/DisappearedNumbers.java
@@ -0,0 +1,34 @@
+//Problem: given an array of length and numbers in range 1-n where some numbers are missing find those numbers
+//link: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+//Source: LeetCode(448)
+//Topics: Arrays
+//Diff: Easy
+
+import java.util.*;
+
+public class DisappearedNumbers {
+    private static List<Integer> naive(int[] nums) {
+        //20ms, 30.93;
+        //60.95 MB, 9.98;
+        List<Integer> res = new ArrayList<>();
+        HashSet<Integer> x = new HashSet<>();
+        for (int i : nums) {
+            x.add(i);
+        }
+        for (int i = 1; i <= nums.length; i++) {
+            if (!x.contains(i)) {
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+    private static List<Integer> optimised(int[] nums) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,3,2,7,8,2,3,1};
+        System.out.println(naive(nums));
+    }
+}
---
+++ bit_manipulation/MissingNumber.java
@@ -0,0 +1,38 @@
+//Problem: given an array of length and numbers in range 0-n where one number is missing find that number
+//link: https://leetcode.com/problems/missing-number/
+//Source: LeetCode(268)
+//Topics: Bit Manipulation
+//Diff: Easy
+
+
+import java.util.Arrays;
+
+public class MissingNumber {
+    static int naive(int[] nums) {
+        //5ms, 25.1;
+        //45.49 MB, 43.38;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    static int optimised(int[] nums) {
+        //0ms, best;
+        //45.4 MB, 56.41;
+        int n = nums.length;
+        int sum = n*(n+1)/2;
+        for (int i : nums) {
+            sum -= i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {9,6,4,2,3,5,7,8,1};
+        System.out.println(optimised(nums));
+    }
+}
